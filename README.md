4409ce4f3605dfdde8cfd1754e283c07d80cae3a
aman <aman@aman>
2023-02-15 16:35:22 +0530

    idk

---
+++ README.md
@@ -0,0 +1,3 @@
+# gitpractice
+# gitpractice
+# gitpractice
---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("init cut now dev pull")
+console.log("initial local repo")
+console.log("Doing changes for pull requests cli")
